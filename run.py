7d504bf5c8b8be2b21c3dcbef82638415dab6698
snowfake99 <67030043@kmitl.ac.th>
2025-10-09 16:22:43 +0700

    commit

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5001))  # ✅ ดึง PORT จาก Railway ถ้ามี ถ้าไม่มีใช้ 5001
+    port = int(os.environ.get("PORT", 5000))  # ✅ ดึง PORT จาก Railway ถ้ามี ถ้าไม่มีใช้ 5001
     app.run(host='0.0.0.0', port=port, debug=False)
