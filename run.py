b6164f37266f8514b2678e804f4c4d5dc97400a5
snowfake99 <67030043@kmitl.ac.th>
2025-10-09 15:24:22 +0700

    2322332

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # ✅ ใช้ PORT จาก Railway
+    port = int(os.environ.get("PORT", 5001))  # ✅ ใช้ PORT จาก Railway
     app.run(host="0.0.0.0", port=port)
